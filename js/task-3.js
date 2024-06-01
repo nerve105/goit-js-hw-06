b6d024abb7c918ce34d11f7f491b1d718f66d9b0
Anna <anna.yevchuk26@gmail.com>
2024-06-01 22:00:04 +0200

    start

---
+++ js/task-3.js
@@ -0,0 +1,15 @@
+
+
+
+
+
+
+
+const builder = new StringBuilder(".");
+console.log(builder.getValue()); // "."
+builder.padStart("^");
+console.log(builder.getValue()); // "^."
+builder.padEnd("^");
+console.log(builder.getValue()); // "^.^"
+builder.padBoth("=");
+console.log(builder.getValue()); // "=^.^="
